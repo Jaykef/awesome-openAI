e054d0a79f15529ba5d10cd586d14f7546ca5d7e
Jaward Sesay <jkef96@icloud.com>
2023-01-27 11:23:45 +0800

    Create grammer_correction.js

---
+++ Examples/Grammar_Correction/grammer_correction.js
@@ -0,0 +1,16 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Correct this to standard English:\n\nShe no went to the market.",
+  temperature: 0,
+  max_tokens: 60,
+  top_p: 1.0,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.0,
+});
