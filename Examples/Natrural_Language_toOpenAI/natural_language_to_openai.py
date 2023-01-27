6807c98b55d08c472717540b29ee39b80c371b09
Jaward Sesay <jkef96@icloud.com>
2023-01-27 11:35:32 +0800

    Create natural_language_to_openai.py

---
+++ Examples/Natrural_Language_toOpenAI/natural_language_to_openai.py
@@ -0,0 +1,18 @@
+# Natural Language to openAI
+# Create code to call to the OpenAI API using a natural language instruction.
+
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  model="code-davinci-002",
+  prompt="\"\"\"\nUtil exposes the following:\nutil.openai() -> authenticates & returns the openai module, which has the following functions:\nopenai.Completion.create(\n    prompt=\"<my prompt>\", # The prompt to start completing from\n    max_tokens=123, # The max number of tokens to generate\n    temperature=1.0 # A measure of randomness\n    echo=True, # Whether to return the prompt in addition to the generated completion\n)\n\"\"\"\nimport util\n\"\"\"\nCreate an OpenAI completion starting from the prompt \"Once upon an AI\", no more than 5 tokens. Does not include the prompt.\n\"\"\"\n",
+  temperature=0,
+  max_tokens=64,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0,
+  stop=["\"\"\""]
+)
