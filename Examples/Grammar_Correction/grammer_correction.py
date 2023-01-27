3149b0063fce65381d460e3142aae5a3b633012b
Jaward Sesay <jkef96@icloud.com>
2023-01-27 11:23:09 +0800

    Create grammer_correction.py

---
+++ Examples/Grammar_Correction/grammer_correction.py
@@ -0,0 +1,14 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Correct this to standard English:\n\nShe no went to the market.",
+  temperature=0,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
