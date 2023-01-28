3e0045d4dbeb2808d00a1d9a041fc47f11b5b3c0
Jaward Sesay <jkef96@icloud.com>
2023-01-28 12:57:48 +0800

    Create chat.py

---
+++ Examples/Chat/chat.py
@@ -0,0 +1,15 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
+  temperature=0.9,
+  max_tokens=150,
+  top_p=1,
+  frequency_penalty=0.0,
+  presence_penalty=0.6,
+  stop=[" Human:", " AI:"]
+)
