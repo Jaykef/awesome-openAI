6261b8d4f23add5510a7901c4646730af2072f43
Jaward Sesay <jkef96@icloud.com>
2023-01-28 12:57:14 +0800

    Create chat.js

---
+++ Examples/Chat/chat.js
@@ -0,0 +1,19 @@
+// Open ended conversation with an AI assistant
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
+  temperature: 0.9,
+  max_tokens: 150,
+  top_p: 1,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.6,
+  stop: [" Human:", " AI:"],
+});
