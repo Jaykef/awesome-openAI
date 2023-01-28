33e44360e4c3e360bceccfbbc758b5753b9976e8
Jaward Sesay <jkef96@icloud.com>
2023-01-28 12:59:58 +0800

    Create create_studynotes.py

---
+++ Examples/Study_Notes/create_studynotes.py
@@ -0,0 +1,14 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="What are 5 key points I should know when studying Ancient Rome?",
+  temperature=0.3,
+  max_tokens=150,
+  top_p=1.0,
+  frequency_penalty=0.0,
+  presence_penalty=0.0
+)
