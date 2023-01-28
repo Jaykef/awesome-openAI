1d2e08b5369dabccf8aa03fbd4101af9a232a0cf
Jaward Sesay <jkef96@icloud.com>
2023-01-28 12:50:46 +0800

    Create summarize_notes.js

---
+++ Examples/SummarizeNotes/summarize_notes.js
@@ -0,0 +1,18 @@
+// Turn meeting notes into a summary
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Convert my short hand into a first-hand account of the meeting:\n\nTom: Profits up 50%\nJane: New servers are online\nKjel: Need more time to fix software\nJane: Happy to help\nParkman: Beta testing almost done",
+  temperature: 0,
+  max_tokens: 64,
+  top_p: 1.0,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.0,
+});
