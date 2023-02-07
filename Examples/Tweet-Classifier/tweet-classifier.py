5bc05d49cd89770d6b2748c1a8d505f28e27636a
Jaward Sesay <jkef96@icloud.com>
2023-02-07 11:50:53 +0800

    Create tweet-classifier.py

---
+++ Examples/Tweet-Classifier/tweet-classifier.py
@@ -0,0 +1,14 @@
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="Decide whether a Tweet's sentiment is positive, neutral, or negative.\n\nTweet: \"I loved the new Batman movie!\"\nSentiment:",
+  temperature=0,
+  max_tokens=60,
+  top_p=1.0,
+  frequency_penalty=0.5,
+  presence_penalty=0.0
+)
